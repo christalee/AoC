b20f54b25ecef1b29325271817bf165fe381cd79
Christalee Bieber <cbieber@alum.mit.edu>
2018-12-27 10:35:56 -0500

    preparing to make progress / tests

---
+++ aoc_2017_code.py
@@ -0,0 +1,5 @@
+# Christalee Bieber, 2018
+# cbieber@alum.mit.edu
+
+# Advent of Code 2017
+# http://adventofcode.com/2017/
--- day1.py
+++
@@ -1,4 +0,0 @@
-# Christalee Bieber, 2017
-# cbieber@alum.mit.edu
-# http://adventofcode.com/2017/day/1
-
