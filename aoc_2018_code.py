94e592f9f02519d28dae28974aea58afcd36e3ad
Christalee Bieber <cbieber@alum.mit.edu>
2019-01-30 17:59:11 -0500

    code / tests for days 1, 4

--- aoc_2018_code.py
+++ aoc_2018_code.py
@@ -6,6 +6,7 @@
 
 from datetime import datetime, timedelta
 
+import numpy as np
 import pandas as pd
 
 
@@ -16,12 +17,13 @@ def input(filename):
     return data
 
 
-def day4(records=None):
+def day4_part2(records=None):
     if not records:
         records = input('day4.txt')
 
     records.sort()
-    history = {}
+    history = pd.DataFrame(columns=['day', 'guard'])
+    minutes = pd.DataFrame(np.zeros((1, 60), dtype=int), columns=np.arange(60))
 
     for r in records:
         s = r.split()
@@ -29,11 +31,66 @@ def day4(records=None):
             '[]'), '%Y-%m-%d %H:%M') + timedelta(days=36500 * 5)
         day = pd.Timestamp(ts).round('D').date()
         if 'Guard' in r:
-            history[day] = {'guard': s[3]}
-            # also create an hour's worth of np.zero
-            # use a pd.DataFrame instead of a dictionary
-        if day in history:
-            history[day][ts.minute] = ' '.join([s[2], s[3]])
+            result = pd.concat(
+                [pd.DataFrame([{'day': day, 'guard': s[3]}]), minutes], axis=1)
+            history = pd.concat([history, result], sort=True)
+        if 'falls asleep' in r:
+            history.loc[history['day'] == day, list(range(ts.minute, 60))] = 1
+        if 'wakes up' in r:
+            history.loc[history['day'] == day, list(range(ts.minute, 60))] = 0
+
+    history['total'] = history[np.arange(60)].sum(axis=1)
+
+    # by_guards = history[['guard', 'total']].groupby('guard').sum()
+    # g = by_guards.idxmax()[0]
+    # guard_minutes = history.loc[history['guard'] == g, np.arange(60)].sum()
+    # m = guard_minutes.idxmax()
+    #
+    # print('Part 1\nguard: ' + g + ', minute: ' + str(m))
+
+    by_minutes = history[['guard', *np.arange(60)]].groupby('guard').sum()
+    top_minute = by_minutes.max().idxmax()
+    top_guard = by_minutes[[top_minute]].idxmax().item()
+
+    return int(top_guard.strip('#')) * top_minute
+
+
+def day4_part1(records=None):
+    if not records:
+        records = input('day4.txt')
+
+    records.sort()
+    history = pd.DataFrame(columns=['day', 'guard'])
+    minutes = pd.DataFrame(np.zeros((1, 60), dtype=int), columns=np.arange(60))
+
+    for r in records:
+        s = r.split()
+        ts = datetime.strptime(' '.join([s[0], s[1]]).strip(
+            '[]'), '%Y-%m-%d %H:%M') + timedelta(days=36500 * 5)
+        day = pd.Timestamp(ts).round('D').date()
+        if 'Guard' in r:
+            result = pd.concat(
+                [pd.DataFrame([{'day': day, 'guard': s[3]}]), minutes], axis=1)
+            history = pd.concat([history, result], sort=True)
+        if 'falls asleep' in r:
+            history.loc[history['day'] == day, list(range(ts.minute, 60))] = 1
+        if 'wakes up' in r:
+            history.loc[history['day'] == day, list(range(ts.minute, 60))] = 0
+
+    history['total'] = history[np.arange(60)].sum(axis=1)
+
+    by_guards = history[['guard', 'total']].groupby('guard').sum()
+    g = by_guards.idxmax()[0]
+    guard_minutes = history.loc[history['guard'] == g, np.arange(60)].sum()
+    m = guard_minutes.idxmax()
+
+    return int(g.strip('#')) * m
+
+    # by_minutes = history[['guard', *np.arange(60)]].groupby('guard').sum()
+    # top_minute = by_minutes.max().idxmax()
+    # top_guard = by_minutes[[top_minute]].idxmax().item()
+    #
+    # print('Part 2\nguard: ' + top_guard + ', minute: ' + str(top_minute))
 
 
 def day1_part2(frequencies=None):
--- aoc_2018_test.py
+++ aoc_2018_test.py
@@ -25,16 +25,20 @@ def test_day4():
         '[1518-11-02 00:50] wakes up',
         '[1518-11-03 00:05] Guard #10 begins shift',
         '[1518-11-03 00:24] falls asleep',
-        '[1518-11-03 00:29] wakes up'
+        '[1518-11-03 00:29] wakes up',
         '[1518-11-04 00:02] Guard #99 begins shift',
         '[1518-11-04 00:36] falls asleep',
-        '[1518-11-04 00:46] wakes up'
-        '[1518-11-05 00:03] Guard #99 begins shift'
-        '[1518-11-05 00:45] falls asleep'
-        '[1518-11-05 00:55] wakes up]'
+        '[1518-11-04 00:46] wakes up',
+        '[1518-11-05 00:03] Guard #99 begins shift',
+        '[1518-11-05 00:45] falls asleep',
+        '[1518-11-05 00:55] wakes up'
     ]
 
-    # assert day4(example) == 240
+    assert day4_part1(example) == 240
+    assert day4_part2(example) == 4455
+
+    assert day4_part1() == 39698
+    assert day4_part2() == 14920
 
 
 def test_day3():
@@ -50,9 +54,11 @@ def test_day1():
     assert day1_part1([+1, +1, +1]) == 3
     assert day1_part1([+1, +1, -2]) == 0
     assert day1_part1([-1, -2, -3]) == -6
+    assert day1_part1() == 585
 
     assert day1_part2([+1, -2, +3, +1]) == 2
     assert day1_part2([+1, -1]) == 0
     assert day1_part2([+3, +3, +4, -2, -4]) == 10
     assert day1_part2([-6, +3, +8, +5, -6]) == 5
     assert day1_part2([+7, +7, -2, -7, -4]) == 14
+    assert day1_part2() == 83173
